c47d8f61744116437e14a7fe5f14988fc3beda24
Mike Stott <unwrittenmike@gmail.com>
2023-04-13 08:47:06 +0000

    Added 418 route

--- app.js
+++ app.js
@@ -7,5 +7,11 @@ app.get('/', (req,res) => {
 	res.send("Hello World! I'm alive.");
 });
 
+app.get("/418", (req, res) => {
+  res.status(418);
+  res.set("Custom-Header", "Hi!");
+  res.send("Coffee!");
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
